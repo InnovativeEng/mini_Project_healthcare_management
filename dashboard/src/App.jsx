c75fa8c1b03dfadef5c99732327e4aeefa3fd1f2
Ajay kumar verma <vermaajay1726@gmail.com>
2025-12-03 11:37:11 +0530

    Update App.  url change for render

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -26,7 +26,8 @@ const App = () => {
     const fetchUser = async () => {
       try {
         const response = await axios.get(
-          "http://localhost:4000/api/v1/user/admin/me",
+          "https://healthcare-management-backend-yept.onrender.com/api/v1/user/admin/me",
+
           {
             withCredentials: true,
           }
