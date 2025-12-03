da16b0ab2bbb71bac517276c959e1c2d73f1859f
Ajay kumar verma <vermaajay1726@gmail.com>
2025-12-03 12:43:07 +0530

    Update Navbar.jsx URL by render URL

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -11,7 +11,8 @@ const Navbar = () => {
 
   const handleLogout = async () => {
     await axios
-      .get("http://localhost:4000/api/v1/user/patient/logout", {
+      .get("https://healthcare-management-backend-yept.onrender.com/api/v1/user/patient/logout",
+ {
         withCredentials: true,
       })
       .then((res) => {
